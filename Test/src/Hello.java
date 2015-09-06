0e93dc58dbcb3348449046975f3f085845d389b5
Pette <petrit_gerdovci@hotmail.com>
2015-09-06 15:34:53 +0200

    reset this

--- Test/src/Hello.java
+++ Test/src/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hej");
+
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("RESET");
+		}
+
 	}
 
 }
