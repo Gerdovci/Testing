3ded110cc337f3534ae4e8be97b3eff53b5fb82d
Pette <petrit_gerdovci@hotmail.com>
2015-09-14 20:57:12 +0200

    new comit

--- Test/src/SimpleAgent.java
+++ Test/src/SimpleAgent.java
@@ -1,42 +1,44 @@
-import javax.management.*;
-import java.lang.management.*;
- 
+import java.lang.management.ManagementFactory;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
 public class SimpleAgent {
-   private MBeanServer mbs = null;
- 
-   public SimpleAgent() {
- 
-      // Get the platform MBeanServer
-       mbs = ManagementFactory.getPlatformMBeanServer();
- 
-      // Unique identification of MBeans
-      Hello helloBean = new Hello();
-      ObjectName helloName = null;
- 
-      try {
-         // Uniquely identify the MBeans and register them with the platform MBeanServer 
-         helloName = new ObjectName("FOO:name=HelloBean");
-         mbs.registerMBean(helloBean, helloName);
-      } catch(Exception e) {
-         e.printStackTrace();
-      }
-   }
- 
-   // Utility method: so that the application continues to run
-   private static void waitForEnterPressed() {
-      try {
-         System.out.println("Press  to continue...");
-         System.in.read();
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
+    private MBeanServer mbs = null;
+
+    public SimpleAgent() {
+	System.out.println("testing");
+	// Get the platform MBeanServer
+	mbs = ManagementFactory.getPlatformMBeanServer();
+
+	// Unique identification of MBeans
+	Hello helloBean = new Hello();
+	ObjectName helloName = null;
+
+	try {
+	    // Uniquely identify the MBeans and register them with the platform
+	    // MBeanServer
+	    helloName = new ObjectName("FOO:name=HelloBean");
+	    mbs.registerMBean(helloBean, helloName);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
     }
- 
-   public static void main(String argv[]) {
-      SimpleAgent agent = new SimpleAgent();
-      System.out.println("SimpleAgent is running...");
-      SimpleAgent.waitForEnterPressed();
-   }
-   
-   
+
+    // Utility method: so that the application continues to run
+    private static void waitForEnterPressed() {
+	try {
+	    System.out.println("Press  to continue...");
+	    System.in.read();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public static void main(String argv[]) {
+	SimpleAgent agent = new SimpleAgent();
+	System.out.println("SimpleAgent is running...");
+	SimpleAgent.waitForEnterPressed();
+    }
+
 }
